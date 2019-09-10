0f7e82445603974b9760a5362b96c791068b6040
MinSu-Kim <net94.teacher73@gmail.com>
2019-09-11 00:37:10 +0900

    numpy test 추가

---
+++ python_tutorial/numpy/numpy_test.py
@@ -0,0 +1,15 @@
+import numpy as np
+a = [1,2,3]
+b = np.array(a)
+print(type(b))
+
+data1 = [0,1,2,3,4,5]
+a1 = np.array(data1)
+print(a1)
+
+print(a1.dtype)
+
+a2 = np.arange(0, 10, 2)
+print(a2)
+
+print(np.arange(5))
