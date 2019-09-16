234ee39581f3520f55501fd67eb9b793f0d8f2ff
영남인재교사 <net94.teacher@gmail.com>
2019-09-16 18:03:36 +0900

    https://wikidocs.net/21927 추가함

---
+++ pyqt5_tutorial/chap02/chap02_02.py
@@ -0,0 +1,21 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("Icon")
+        self.setWindowIcon(QIcon('../data/web.png'))    # setWindowIcon() 메서드는 어플리케이션 아이콘을 설정
+        self.setGeometry(300, 300, 300, 200)            # setGeometry() 메서드는 창의 위치와 크기를 설정 move()와 resize() 메서드를 하나로 합쳐놓은 것과 같음
+        self.show()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec())
