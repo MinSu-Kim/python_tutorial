078d9ad5d18a79c698f081d2e552b870e9fb6ec4
영남인재교사 <net94.teacher@gmail.com>
2019-09-19 17:47:18 +0900

    json file 처리

---
+++ pyqt5_tutorial/model-view/TodoModel.py
@@ -0,0 +1,84 @@
+import json
+
+from PyQt5 import QtCore, QtWidgets, uic, QtGui
+from PyQt5.QtCore import Qt
+
+qt_creator_file = "mainwindow.ui"
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qt_creator_file)
+tick = QtGui.QImage('../../icons/tick.png')
+
+
+class TodoModel(QtCore.QAbstractListModel):
+    def __init__(self, todos=None):
+        super().__init__()
+        self.todos = todos or []
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            _, text = self.todos[index.row()]
+            return text
+        if role == Qt.DecorationRole:
+            status, _ = self.todos[index.row()]
+            if status:
+                return tick
+
+    def rowCount(self, index):
+        return len(self.todos)
+
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        self.model = TodoModel(todos=[(False, 'my first todo')])
+        self.load()
+        self.todoView.setModel(self.model)
+        self.addButton.pressed.connect(self.add)
+        self.deleteButton.pressed.connect(self.delete)
+        self.completeButton.pressed.connect(self.complete)
+
+    def add(self):
+        text = self.todoEdit.text()
+        if text:
+            self.model.todos.append((False, text))
+            self.model.layoutChanged.emit()
+            self.todoEdit.setText("")
+            self.save()
+
+    def delete(self):
+        indexes = self.todoView.selectedIndexes()
+        if indexes:
+            index = indexes[0]
+            del self.model.todos[index.row()]
+            self.model.layoutChanged.emit()
+            self.todoView.clearSelection()
+            self.save()
+
+    def complete(self):
+        indexes = self.todoView.selectedIndexes()
+        if indexes:
+            index = indexes[0]
+            row = index.row()
+            status, text = self.model.todos[row]
+            self.model.todos[row] = (True, text)
+            self.model.dataChanged.emit(index, index)
+            self.todoView.clearSelection()
+            self.save()
+
+    def load(self):
+        try:
+            with open('data.db', 'r') as f:
+                self.model.todos = json.load(f)
+        except Exception:
+            pass
+
+    def save(self):
+        with open('data.db', 'w') as f:
+            data = json.dump(self.model.todos, f)
+
+
+app = QtWidgets.QApplication([])
+window = MainWindow()
+window.show()
+app.exec_()
