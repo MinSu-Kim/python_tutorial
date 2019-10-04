0ce720b0b7e6a1d363af91537c084f068028ca5b
MinSu-Kim <net94.teacher73@gmail.com>
2019-10-04 22:46:11 +0900

    http://codetorial.net/signal_slot/event_handler.html 부터 시작(signal_study)

---
+++ pyqt5_tutorial/signal_slot/signal_study01.py
@@ -0,0 +1,30 @@
+# 다이얼의 값이 변할 때 발생하는 시그널이 LCD 화면에 숫자를 표시하는 슬롯과 연결
+from PyQt5.QtWidgets import QApplication, QWidget, QLCDNumber, QDial, QVBoxLayout
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        lcd = QLCDNumber(self)  # LCD 화면과 같이 숫자를 표시
+        dial = QDial(self)      # QDial은 다이얼을 회전해서 값을 조절하는 위젯
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(lcd)
+        vbox.addWidget(dial)
+        self.setLayout(vbox)
+
+        dial.valueChanged.connect(lcd.display)
+        print(dir(lcd))
+        self.setWindowTitle('Signal and Slot')
+        self.setGeometry(300, 300, 200, 200)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    ex = MyApp()
+    app.exec_()
