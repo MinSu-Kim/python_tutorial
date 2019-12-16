69a70b9fd33a47195863f4bc65edbba92eb59402
teacher <net94.teacher@gmail.com>
2019-12-16 11:26:47 +0900

    Embedding matplotlib in PyQt5  PyQt5 를 이용해 위젯내에 matplotlib 를 이용해 만든 그래프를 넣는 과정 테스트 완료

--- matplotlib_tutorial/embded_matplot.py
+++ matplotlib_tutorial/embded_matplot.py
@@ -1,49 +1,84 @@
 import sys
-import time
 
+import matplotlib.pyplot as plt
 import numpy as np
-
-from matplotlib.backends.qt_compat import QtCore, QtWidgets, is_pyqt5
-from matplotlib.backends.backend_qt5agg import FigureCanvas, NavigationToolbar2QT as NavigationToolbar
-
-from matplotlib.figure import Figure
+from PyQt5.QtWidgets import *
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 
-class ApplicationWindow(QtWidgets.QMainWindow):
+class MyWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self._main = QtWidgets.QWidget()
-        self.setCentralWidget(self._main)
-        layout = QtWidgets.QVBoxLayout(self._main)
+        self.initUI()
 
-        static_canvas = FigureCanvas(Figure(figsize=(5, 3)))
-        layout.addWidget(static_canvas)
-        self.addToolBar(NavigationToolbar(static_canvas, self))
+        self.setLayout(self.layout)
+        self.setGeometry(200, 200, 800, 600)
 
-        dynamic_canvas = FigureCanvas(Figure(figsize=(5, 3)))
-        layout.addWidget(dynamic_canvas)
-        self.addToolBar(QtCore.Qt.BottomToolBarArea,
-                        NavigationToolbar(dynamic_canvas, self))
+    def initUI(self):
+        self.fig = plt.Figure()
+        self.canvas = FigureCanvas(self.fig)
 
-        self._static_ax = static_canvas.figure.subplots()
-        t = np.linspace(0, 10, 501)
-        self._static_ax.plot(t, np.tan(t), ".")
+        layout = QVBoxLayout()
+        layout.addWidget(self.canvas)
 
-        self._dynamic_ax = dynamic_canvas.figure.subplots()
-        self._timer = dynamic_canvas.new_timer(
-            100, [(self._update_canvas, (), {})])
-        self._timer.start()
+        cb = QComboBox()
+        cb.addItem('Graph1')
+        cb.addItem('Graph2')
+        cb.activated[str].connect(self.onComboBoxChanged)
+        layout.addWidget(cb)
 
-    def _update_canvas(self):
-        self._dynamic_ax.clear()
-        t = np.linspace(0, 10, 101)
-        # Shift the sinusoid as a function of time.
-        self._dynamic_ax.plot(t, np.sin(t + time.time()))
-        self._dynamic_ax.figure.canvas.draw()
+        self.layout = layout
+
+        self.onComboBoxChanged(cb.currentText())
+
+    def onComboBoxChanged(self, text):
+        if text == 'Graph1':
+            self.doGraph1()
+        elif text == 'Graph2':
+            self.doGraph2()
+
+    def doGraph1(self):
+        x = np.arange(0, 10, 0.5)
+        y1 = np.sin(x)
+        y2 = np.cos(x)
+
+        self.fig.clear()
+
+        ax = self.fig.add_subplot(111)
+        ax.plot(x, y1, label="sin(x)")
+        ax.plot(x, y2, label="cos(x)", linestyle="--")
+
+        ax.set_xlabel("x")
+        ax.set_xlabel("y")
+
+        ax.set_title("sin & cos")
+        ax.legend()
+        ax.grid()
+
+        self.canvas.draw()
+
+    def doGraph2(self):
+        x = np.arange(0, 20, 0.5)
+        y1 = np.sin(x)
+        y2 = np.cos(x)
+
+        self.fig.clear()
+
+        ax = self.fig.add_subplot(111)
+        ax.plot(x, y1, label="sin(x)")
+        ax.plot(x, y2, label="cos(x)", linestyle="--")
+
+        ax.set_xlabel("x")
+        ax.set_xlabel("y")
+
+        ax.set_title("sin & cos")
+        ax.legend()
+
+        self.canvas.draw()
 
 
 if __name__ == "__main__":
-    qapp = QtWidgets.QApplication(sys.argv)
-    app = ApplicationWindow()
-    app.show()
-    qapp.exec_()
+    app = QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    app.exec_()
